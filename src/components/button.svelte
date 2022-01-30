957d2889f29836e299fb418d215dce9e2ad22ee7
Min-Hyeok <alsgur0008@naver.com>
2022-01-30 22:20:55 +0900

    first commit

---
+++ src/components/button.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = 0;
+
+  function add() {
+    count = count + 1;
+  }
+</script>
+<div>
+  <button on:click="{add}" type="button">클릭!</button>
+  <p>{count}</p>
+</div>
+<style>
+  
+</style>
